f6f31a792ec05b2cdc26fb69c3d3310d5e4fd1bb
Penny Chang <yahusanchang@gmail.com>
2022-10-30 01:28:41 +0800

    Navbar大螢幕切版

---
+++ src/components/Navbar.js
@@ -0,0 +1,245 @@
+import React from 'react'
+import styled from 'styled-components'
+import SearchFilter from './SearchFilter.js'
+import '../Basic.css'
+
+
+const Element = ({ red, className }) => {
+	return (
+		<div className={className + ' col-md-4 h-100 p-4'}>
+      <SearchFilter/>
+      <div className='card-list'>
+        <div className='card'>
+          <div className='p-3'>
+            <h3 className='main-title'>內湖停車場</h3>
+            <div className='info-group d-flex align-items-center mb-1'>
+              <i className="fa-solid fa-location-dot"></i>
+              <p className='info'>114台北市內湖區金湖路46巷</p>
+            </div>
+            <div className='info-group d-flex align-items-center mb-1'>
+              <i className="fa-solid fa-map-pin"></i>
+              <div className='info'><span>200</span>公尺</div>
+            </div>
+            <div className='card-body p-0'>
+              <p>為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。</p>
+            </div>
+          </div>
+          <div className='card-footer d-flex'>
+            <div className='info-group d-flex align-items-center'>
+              <div className='icon-wrap'><i className="fa-solid fa-car"></i></div>
+              <p className='info'>1</p>
+            </div>
+            <div className='info-group d-flex align-items-center'>
+              <div className='icon-wrap'><i className="fa-solid fa-motorcycle"></i></div>
+              <p className='info'>10</p>
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='p-3'>
+            <h3 className='main-title'>內湖停車場</h3>
+            <div className='info-group d-flex align-items-center mb-1'>
+              <i className="fa-solid fa-location-dot"></i>
+              <p className='info'>114台北市內湖區金湖路46巷</p>
+            </div>
+            <div className='info-group d-flex align-items-center mb-1'>
+              <i className="fa-solid fa-map-pin"></i>
+              <div className='info'><span>200</span>公尺</div>
+            </div>
+            <div className='card-body p-0'>
+              <p>為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。</p>
+            </div>
+          </div>
+          <div className='card-footer d-flex'>
+            <div className='info-group d-flex align-items-center'>
+              <div className='icon-wrap'><i className="fa-solid fa-car"></i></div>
+              <p className='info'>1</p>
+            </div>
+            <div className='info-group d-flex align-items-center'>
+              <div className='icon-wrap'><i className="fa-solid fa-motorcycle"></i></div>
+              <p className='info'>10</p>
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='p-3'>
+            <h3 className='main-title'>內湖停車場</h3>
+            <div className='info-group d-flex align-items-center mb-1'>
+              <i className="fa-solid fa-location-dot"></i>
+              <p className='info'>114台北市內湖區金湖路46巷</p>
+            </div>
+            <div className='info-group d-flex align-items-center mb-1'>
+              <i className="fa-solid fa-map-pin"></i>
+              <div className='info'><span>200</span>公尺</div>
+            </div>
+            <div className='card-body p-0'>
+              <p>為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。</p>
+            </div>
+          </div>
+          <div className='card-footer d-flex'>
+            <div className='info-group d-flex align-items-center'>
+              <div className='icon-wrap'><i className="fa-solid fa-car"></i></div>
+              <p className='info'>1</p>
+            </div>
+            <div className='info-group d-flex align-items-center'>
+              <div className='icon-wrap'><i className="fa-solid fa-motorcycle"></i></div>
+              <p className='info'>10</p>
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='p-3'>
+            <h3 className='main-title'>內湖停車場</h3>
+            <div className='info-group d-flex align-items-center mb-1'>
+              <i className="fa-solid fa-location-dot"></i>
+              <p className='info'>114台北市內湖區金湖路46巷</p>
+            </div>
+            <div className='info-group d-flex align-items-center mb-1'>
+              <i className="fa-solid fa-map-pin"></i>
+              <div className='info'><span>200</span>公尺</div>
+            </div>
+            <div className='card-body p-0'>
+              <p>為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。</p>
+            </div>
+          </div>
+          <div className='card-footer d-flex'>
+            <div className='info-group d-flex align-items-center'>
+              <div className='icon-wrap'><i className="fa-solid fa-car"></i></div>
+              <p className='info'>1</p>
+            </div>
+            <div className='info-group d-flex align-items-center'>
+              <div className='icon-wrap'><i className="fa-solid fa-motorcycle"></i></div>
+              <p className='info'>10</p>
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='p-3'>
+            <h3 className='main-title'>內湖停車場</h3>
+            <div className='info-group d-flex align-items-center mb-1'>
+              <i className="fa-solid fa-location-dot"></i>
+              <p className='info'>114台北市內湖區金湖路46巷</p>
+            </div>
+            <div className='info-group d-flex align-items-center mb-1'>
+              <i className="fa-solid fa-map-pin"></i>
+              <div className='info'><span>200</span>公尺</div>
+            </div>
+            <div className='card-body p-0'>
+              <p>為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。</p>
+            </div>
+          </div>
+          <div className='card-footer d-flex'>
+            <div className='info-group d-flex align-items-center'>
+              <div className='icon-wrap'><i className="fa-solid fa-car"></i></div>
+              <p className='info'>1</p>
+            </div>
+            <div className='info-group d-flex align-items-center'>
+              <div className='icon-wrap'><i className="fa-solid fa-motorcycle"></i></div>
+              <p className='info'>10</p>
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='p-3'>
+            <h3 className='main-title'>內湖停車場</h3>
+            <div className='info-group d-flex align-items-center mb-1'>
+              <i className="fa-solid fa-location-dot"></i>
+              <p className='info'>114台北市內湖區金湖路46巷</p>
+            </div>
+            <div className='info-group d-flex align-items-center mb-1'>
+              <i className="fa-solid fa-map-pin"></i>
+              <div className='info'><span>200</span>公尺</div>
+            </div>
+            <div className='card-body p-0'>
+              <p>為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。</p>
+            </div>
+          </div>
+          <div className='card-footer d-flex'>
+            <div className='info-group d-flex align-items-center'>
+              <div className='icon-wrap'><i className="fa-solid fa-car"></i></div>
+              <p className='info'>1</p>
+            </div>
+            <div className='info-group d-flex align-items-center'>
+              <div className='icon-wrap'><i className="fa-solid fa-motorcycle"></i></div>
+              <p className='info'>10</p>
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='p-3'>
+            <h3 className='main-title'>內湖停車場</h3>
+            <div className='info-group d-flex align-items-center mb-1'>
+              <i className="fa-solid fa-location-dot"></i>
+              <p className='info'>114台北市內湖區金湖路46巷</p>
+            </div>
+            <div className='info-group d-flex align-items-center mb-1'>
+              <i className="fa-solid fa-map-pin"></i>
+              <div className='info'><span>200</span>公尺</div>
+            </div>
+            <div className='card-body p-0'>
+              <p>為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。為地下二層停車場，計有1998個小型車停車格，1337個機車停車位。</p>
+            </div>
+          </div>
+          <div className='card-footer d-flex'>
+            <div className='info-group d-flex align-items-center'>
+              <div className='icon-wrap'><i className="fa-solid fa-car"></i></div>
+              <p className='info'>1</p>
+            </div>
+            <div className='info-group d-flex align-items-center'>
+              <div className='icon-wrap'><i className="fa-solid fa-motorcycle"></i></div>
+              <p className='info'>10</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+	)
+}
+
+
+const Navbar = styled(Element)`
+  box-shadow: rgb(0 0 0 / 25%) -4px 0px 12px 0px;
+  overflow-y: scroll;
+  max-height: 100vh;
+
+  i{color: var(--main-color)}
+
+  .card + .card{
+    margin-top: 20px;
+  }
+
+  .card-body{
+    p{
+      font-size:14px;
+      display:-webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+      overflow:hidden;
+      color: var(--dark-70)
+    }
+  }
+
+  .info-group{
+    .icon-wrap{
+      width:40px;
+      height: 40px;
+      border-radius: 50%;
+      border: 1px solid var(--main-color);
+      color: var(--main-color);
+      text-align: center;
+
+      i{line-height:40px}
+    }
+    .info{margin-left:10px}
+  }
+
+  .card-footer{
+    .info-group + .info-group{
+      margin-left:10px;
+    }
+  }
+`
+
+export default Navbar
+
+
