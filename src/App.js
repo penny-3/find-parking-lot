a41e8161d27946c2e3646d7f01a854f7748068d9
Penny Chang <yahusanchang@gmail.com>
2022-10-29 16:25:42 +0800

    建立檔案、共用css設定

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
-import './App.css'
+import './Basic.css'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import styled from "styled-components"
 import Swal from 'sweetalert2'
 import axios from "axios"
+// import MyMapWrap from "./components/MyMapWrap"
+// import Navbar from "./components/Navbar"
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
-      </div>
+      <main>
+        <div className="row">
+        </div>
+      </main>
     )
   }
 }
