8e112c1d9707394b10c82a18edd1227798767179
Penny Chang <yahusanchang@gmail.com>
2022-10-30 01:26:17 +0800

    建立router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import './Basic.css'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import styled from "styled-components"
 import Swal from 'sweetalert2'
 import axios from "axios"
-// import MyMapWrap from "./components/MyMapWrap"
-// import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar"
+import MapMoto from './components/MapMoto'
+import MapCar from './components/MapCar'
 
-class App extends Component {
-  render () {
-    return (
-      <main>
-        <div className="row">
-        </div>
-      </main>
-    )
-  }
+function Car() {
+  return (
+    <main className='main-view'>
+      <div className='row mx-0'>
+        <Navbar></Navbar>
+        <MapCar></MapCar>
+      </div>
+    </main>
+  );
+}
+
+function Moto() {
+  return (
+    <main className='main-view'>
+      <div className='row mx-0'>
+        <Navbar></Navbar>
+        <MapMoto></MapMoto>
+      </div>
+    </main>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to="/car" replace />}
+        />
+        <Route path="/car" element={<Car />} />
+        <Route path="/moto" element={<Moto />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,13 +8,7 @@ import 'leaflet/dist/leaflet.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
