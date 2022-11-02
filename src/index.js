7304bcb6aabc10741ae9353dabc2c686a937c932
Penny Chang <yahusanchang@gmail.com>
2022-11-02 23:53:26 +0800

    新增StrictMode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
@@ -8,7 +8,11 @@ import 'leaflet/dist/leaflet.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+<StrictMode>
+  <App />
+</StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
